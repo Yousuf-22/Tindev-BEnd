17afa996a33419a05fabbe372295ecbc11c10b34
Yousuff <Yousuf-22>
2025-06-17 09:44:11 +0530

    one

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
-
 const app = express();
-
 const cors = require("cors");
+require("dotenv").config();
+
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -19,7 +19,6 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestsRouter = require("./routes/requests");
 const userRouter = require("./routes/user");
-require("dotenv").config();
 // require("./utils/cronjob.js");
 
 app.use("/", authRouter);
--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   // this returns a Promise
   await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+  
 };
 
 module.exports = connectDB;
